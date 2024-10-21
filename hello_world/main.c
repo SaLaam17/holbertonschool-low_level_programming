e253559a9cf28ef73d53ed2f1db3d52b29d84b2a
SaLaam17 <slaamri@yahoo.fr>
2024-10-21 13:29:11 +0000

    main.c

---
+++ hello_world/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+ * Use the function puts
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void) 
+{
+    puts("Programming is like building a multilingual puzzle\n");
+
+    return (0);
+}
