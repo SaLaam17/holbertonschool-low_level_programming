38f9cfa6225c22d2dc976046c449fc4d35a428f1
SaLaam17 <slaamri@yahoo.fr>
2024-10-21 16:11:40 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,7 +3,11 @@
  * Write a C program that prints exactly
  * \"Programming is like building a multilingual puzzle,
  * followed by a new line.
- * Use the function puts. You are not allowed to use printf
+*/
+/**
+* main - This is the entry point of the code
+* return - 0 Successful
+* puts - output of characters
 */
 int main(void)
 {
