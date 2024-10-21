a7b18fa1d52f08ea586877264497298fc42be47f
SaLaam17 <slaamri@yahoo.fr>
2024-10-21 15:40:57 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,7 +2,6 @@
 /**
  * Write a C program that prints exactly 
  * \"Programming is like building a multilingual puzzle, followed by a new line.
- *
  * Use the function puts. You are not allowed to use printf
 */
 int main(void)
