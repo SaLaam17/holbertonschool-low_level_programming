d2dc32c3af1a2bb028c50856f92905e5f91dab26
SaLaam17 <slaamri@yahoo.fr>
2024-10-22 07:51:41 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -5,8 +5,10 @@
 * Description: Write a C program that prints exactly
 * \"Programming is like building a multilingual puzzle,
 * followed by a new line.
+*
 * puts: output of characters
-* return: (0) (Success)
+* 
+* return: (0) (Success), non-zero (otherwise)
 */
 int main(void)
 {
