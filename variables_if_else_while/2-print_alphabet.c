6ca88116d9cb27570fc596e4fabfdeaed0816809
SaLaam17 <slaamri@yahoo.fr>
2024-10-22 22:29:35 +0000

    2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints the alphabet in lowercase.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+	char ch;
+
 	for (char ch = 'a'; ch <= 'z'; ch++)
 	{
-	putchar(ch);
+		putchar(ch);
 	}
 	putchar('\n');
 	return (0);
