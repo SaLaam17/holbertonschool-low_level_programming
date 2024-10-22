6eca9f8f8a233255cf2ab36a69c0d5683f2807d9
SaLaam17 <slaamri@yahoo.fr>
2024-10-22 23:27:20 +0000

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, and then in uppercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'd'; c++)
+		putchar(c);
+
+	for (c = 'f'; c <= 'p'; c++)
+		putchar(c);
+
+	for (c = 'r'; c <= 'z'; c++)
+		putchar(c);
+
+	putchar('\n');
+
+	return (0);
+}
