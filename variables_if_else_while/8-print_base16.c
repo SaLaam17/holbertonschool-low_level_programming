b9609db0c337b8b1a9b382f180cf2c8801c87176
SaLaam17 <slaamri@yahoo.fr>
2024-10-23 16:00:51 +0000

    6-print_numberz.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = '0';
+	char ch = 'a';
+
+	while (n < 10)
+	{
+		putchar(n);
+		n++;
+	}
+	while (ch <= 'f')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
