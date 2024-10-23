9ad60fd918e9dc0b44d30809b23f4fd54940d266
SaLaam17 <slaamri@yahoo.fr>
2024-10-23 07:46:15 +0000

    5-print_numbers.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -10,15 +10,17 @@ int main(void)
 	char c;
 
 	for (c = 'a'; c <= 'd'; c++)
+	{
 		putchar(c);
-
+	}
 	for (c = 'f'; c <= 'p'; c++)
-		putchar(c);
+	{	putchar(c);
 
+	}
 	for (c = 'r'; c <= 'z'; c++)
+	{
 		putchar(c);
-
+	}
 	putchar('\n');
-
 	return (0);
 }
