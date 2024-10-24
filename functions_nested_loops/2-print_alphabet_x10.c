bb3e5012d6e02559c88fbe13af43a29f679ad624
SaLaam17 <slaamri@yahoo.fr>
2024-10-24 23:07:07 +0000

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *
+ *Description: print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ *
+ */
+
+void print_alphabet_x10(void)
+
+{	int ligne;
+	char ch;
+
+	for (ligne = 1; ligne <= 10; ligne++)
+	{
+		ch = 'a';
+		while (ch <= ('z'))
+		{
+			_putchar(ch);
+			ch++;
+		}
+
+	_putchar('\n');
+
+	}
+}
