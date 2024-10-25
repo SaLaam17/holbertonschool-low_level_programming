923efd5a5a3c9cdeabe6d80f9dd40899834794c2
SaLaam17 <slaamri@yahoo.fr>
2024-10-25 13:35:25 +0000

    7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -9,10 +9,13 @@
 
 int print_last_digit(int n)
 {
-	n = n % 10;
-	_putchar('n');
-	_putchar('\n');
-
-	return (0);
+	if (n < 0)
+	{
+		n = -n;
+		n = n % 10;
+		_putchar(n + '0');
+		_putchar('\n');
+	}
+		return (0);
 
 }
