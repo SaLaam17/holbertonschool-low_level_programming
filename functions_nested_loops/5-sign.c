151c0b764652e289157f7c7d31462071b70607e7
SaLaam17 <slaamri@yahoo.fr>
2024-10-25 10:02:16 +0000

    5-sign.c

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - Entry point
+ * @n: input
+ * Description: int _islower checks for lowercase character.
+ * Return: 1, 0, -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (-1);
+	}
+
+}
