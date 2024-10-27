48ce358b5b8e980a42331ff53c57594a1f43b330
SaLaam17 <slaamri@yahoo.fr>
2024-10-27 10:35:20 +0000

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -14,14 +14,25 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-		_putchar((i*j) + '0');
-		if (i == 9)
-			continue;
-
-		_putchar(',');
-		_putchar(' ');
+			if (j == 0)
+			_putchar(i * j + '0');
+			else if (i * j >= 10)
+			{
+			_putchar(' ');
+			_putchar((i * j / 10) + '0');
+			_putchar((i * j % 10) + '0');
+			}
+			else
+			{
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(i * j + '0');
+			}
+			if (j < 9)
+			{
+			_putchar(',');
+			}
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
-	return (0);
 }
