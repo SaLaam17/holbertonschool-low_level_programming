17c4c02e74f1e954599009cc36998d75ae5ac614
SaLaam17 <slaamri@yahoo.fr>
2024-10-27 17:42:19 +0000

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @num1: input
+ * @num2: input
+ * Description: function that adds two integers and returns the result.
+ * Return: value of the add of two integers.
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
