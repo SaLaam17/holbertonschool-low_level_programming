8e79c85b1b80142ccd43866283e65fa6f1656c7b
SaLaam17 <slaamri@yahoo.fr>
2024-10-28 14:33:06 +0000

    6-main.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - Entry point
+ * @n: input
+ * Description: function that draws a straight line in the terminal.
+ */
+
+void print_line(int n)
+{
+	int length;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	if (n > 0)
+	{
+		for (length = 0; length <= n; length++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
