81a498b24a6c3701d1b5f1c42cba660b064fa831
SaLaam17 <slaamri@yahoo.fr>
2024-10-28 09:53:15 +0000

    3-main.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ * Description: function that prints the numbers, from 0 to 9,
+ * followed by a new line.
+ */
+
+void print_numbers(void)
+{
+	char ch;
+
+	for (ch = 0; ch <= 9; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
