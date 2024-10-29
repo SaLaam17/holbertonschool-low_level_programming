0d6ed642c1807cbaadb9653eb04e141b2339f145
SaLaam17 <slaamri@yahoo.fr>
2024-10-29 10:25:59 +0000

    7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -9,14 +9,22 @@
 void print_diagonal(int n)
 {
 	int length;
+	int num_escape;
 
 	if (n > 0)
 	{
-		for (length = 0; length <= n; length++)
+		for (length = 0; length < n; length++)
 		{
-			_putchar(' ');
+			for (num_escape = 0; num_escape < length; num_escape++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
 		}
-		_putchar('\\');
 	}
+	else
+	{
 	_putchar('\n');
+	}
 }
