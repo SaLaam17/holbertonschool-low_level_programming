ac9d59e7cf35a64f0a31615d26c81e6dd71fb7f7
SaLaam17 <slaamri@yahoo.fr>
2024-10-29 19:17:20 +0000

    9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -3,24 +3,28 @@
 /**
  * main - Entry point
  *
- * Description: function that prints a square, followed by a new line.
+ * Description: function that prints the numbers from 1 to 100
+ * followed by a new line.
+ * But for multiples of three print Fizz instead of the number
+ * and for the multiples of five print Buzz.
+ * For numbers which are multiples of both three and five print FizzBuzz.
  * Return: Always 0 Success
  */
 
 int main(void)
 {
-	int num;
+	int n;
 
-	for (num = 1; num <= 100; num++)
+	for (n = 1; n <= 100; n++)
 	{
-		if ((num % 3) == 0 && (num % 5) == 0)
+		if ((n % 3) == 0 && (n % 5) == 0)
 		printf("FizzBuzz ");
-		else if ((num % 3) == 0)
+		else if ((n % 3) == 0)
 		printf("Fizz ");
-		else if ((num % 5) == 0)
+		else if ((n % 5) == 0)
 		printf("Buzz ");
 		else
-		printf("%d ", num);
+		printf("%d ", n);
 	}
 
 	printf("\n");
