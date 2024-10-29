0d8141147ae1b89fa5f0ae5d6e4d09b40f2b12dd
SaLaam17 <slaamri@yahoo.fr>
2024-10-29 22:01:20 +0000

    0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Entry point
+ * @n: input
+ * @*n: input
+ * Return: Always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+
+{
+  int x = 10;
+
+  // Pointer declaration and initialization
+  int * ptr = & x;
+
+  // Printing the current value
+  printf("Value of x = %d\n", * ptr);
+
+
+
+  return 0;
+}
