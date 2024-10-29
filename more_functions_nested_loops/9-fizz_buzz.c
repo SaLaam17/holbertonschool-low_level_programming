d6660069a4f6324a76781b00c8038622ba606fd0
SaLaam17 <slaamri@yahoo.fr>
2024-10-29 15:43:33 +0000

    9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * print_square - Entry point
+ * @n: input
+ * Description: function that prints a square, followed by a new line.
+ */
+
+
+for (n=1;n<=9;n++) {_putchar(n + '0'); _putchar(' ');} for (n=10;n<=99;n++) {_putchar((n /10) + '0'); _putchar((n %10)+ '0'); _putchar(' ');} if (n = = 100) { _putchar((n/100) + '0'); _putchar((n/10) + '0'); _putchar((n%10) + '0');}
