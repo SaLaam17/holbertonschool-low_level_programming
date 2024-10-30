29a958d35590616a0838f0557f297e502c0cd0ec
SaLaam17 <slaamri@yahoo.fr>
2024-10-30 15:09:09 +0000

    1-swap.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * swap_int - Entry point
+ * @a: Pointer to an int
+ * @b: Pointer to an int
+ * Description: function that swaps the values of two integers.
+ */
+
+void swap_int(int *a, int *b)
+{
+
+}
