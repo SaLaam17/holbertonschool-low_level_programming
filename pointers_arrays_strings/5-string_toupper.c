dd650345ad0be14802fc814eac8034c53347009b
SaLaam17 <slaamri@yahoo.fr>
2024-11-03 17:35:11 +0000

    5-string_toupper.c

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -12,12 +12,10 @@ char *string_toupper(char *str)
 {
 	int i = 0;
 
-	while (str[i++])
+	while (str[i])
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
-		{
 		str[i] = str[i] - 32;
-		}
 		i++;
 	}
 	return (str);
