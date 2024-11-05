64bc79ca719e32cd162a3db2444b8fc2c0a9cc84
SaLaam17 <slaamri@yahoo.fr>
2024-11-05 13:40:19 +0000

    3-strspn.c

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -14,17 +14,22 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int num_bytes = 0;
 	int i;
 	int j;
+	int found;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
+		found = 0;
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
 				num_bytes++;
+				found = 1;
+				break;
 			}
-			break;
 		}
+		if (!found)
+		break;
 	}
 	return (num_bytes);
 }
