da0e9ba42d78009566aa7987eaa12ee1dc618a4d
SaLaam17 <slaamri@yahoo.fr>
2024-11-06 09:10:51 +0000

    7-print_chessboard

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Entry point
+ * 
+ * Description: function that prints the chessboard.
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 7; i++)
+	{
+		for (j = 0; j < 7; j++)
+		{
+			_putchar(a[i][i]);
+		}
+		_putchar('\n');
+	}
+}
