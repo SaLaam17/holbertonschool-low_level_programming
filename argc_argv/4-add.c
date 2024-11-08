0f2c3ab29ca03f0cf30ca4a85dda290bb85a3328
SaLaam17 <slaamri@yahoo.fr>
2024-11-08 19:32:01 +0000

    4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -12,19 +12,26 @@
  */
 
 int main(int argc, char *argv[])
-{	
-	int i;
+{
+	int i = 0;
 	int sum = 0;
 
-	if (argc < 1)
+	if (argc <= 1)
 	{
 		printf("0\n");
+		return (0);
 	}
 
-		for (i = 0; i <= argc; i++)
-		{
-			sum = sum + atoi(argv[i]);
-			printf("%d\n", sum);
-		}
+	if (i <= 48 && i >= 57)
+	{
+		printf("ERROR\n");
+		return (1);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		sum = sum + atoi(argv[i]);
+	}
+		printf("%d\n", sum);
 	return (0);
 }
