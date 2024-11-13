861f00bfe537859b88d7cc15778640aafc587b97
SaLaam17 <slaamri@yahoo.fr>
2024-11-13 21:31:04 +0000

    3-array_range.c

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * array_range - Entry point
+ * Description: function that creates an array of integers.
+ *
+ *
+ *
+ */
+
+int *array_range(int min, int max)
+{
+
+}
