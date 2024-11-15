3a37426a2d007d34efd0266f6243cfbd7b82e4cf
SaLaam17 <slaamri@yahoo.fr>
2024-11-15 13:16:51 +0000

    4-main.c 4-new_dog.c dog.h

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,15 +1,20 @@
-#include <stdio.h>
 #include "dog.h"
+#include <stdlib.h>
 
 /**
- * 
- *
- * canif: new dog to be created
+ * new_dog - function that creates a new dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ * Return: the new struct dog to be created
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *canif;
+
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
 
 	canif = malloc(sizeof(dog_t));
 
@@ -19,4 +24,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	(*canif).name = name;
 	(*canif).age = age;
 	(*canif).owner = owner;
+
+	return (canif);
 }
