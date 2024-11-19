857183758f5bd6f7d6649f771f0c31b520b7ae25
SaLaam17 <slaamri@yahoo.fr>
2024-11-19 10:29:45 +0000

    3-op_functions.c

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -50,25 +50,25 @@ int op_div(int a, int b)
 	if (b == 0)
 	{
 		printf("Error\n");
-        	exit (100);
+		exit(100);
 	}
 
 	return (a / b);
 }
 
 /**
- * op_div - function that operate the remainder of the division of two integers
+ * op_mod - function that operate the remainder of the division of two integers
  * @a: input
  * @b: input
- * Return: the remainder of the division of a by b
+ * Return: the remainder of the division of a by b.
  */
 
 int op_mod(int a, int b)
 {
 	if (b == 0)
-        {
-                printf("Error\n");
-                exit (100);
-        }
-        return (a % b);
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
 }
