ebebec87b740b6520dc2c6c4a4d516fce49d4e74
SaLaam17 <slaamri@yahoo.fr>
2024-11-19 10:51:46 +0000

    3-get_op_func.c

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -2,10 +2,13 @@
 #include <stdio.h>
 
 /**
- *
+ * get_op_func - function that selects the correct
+ * function to perform the operation asked by the user
+ * @s: the operator passed as argument to the program
  *
  *
  */
 
 int (*get_op_func(char *s))(int, int);
 {
+}
