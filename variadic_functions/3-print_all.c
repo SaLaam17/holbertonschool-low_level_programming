5f2f09678da57c9d53ac54f429260ab2720069b9
SaLaam17 <slaamri@yahoo.fr>
2024-11-20 20:18:39 +0000

    3-main.c  3-print_all.c

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_all - function that prints anything.
+ *
+ */
+
+void print_all(const char * const format, ...)
+{
+
