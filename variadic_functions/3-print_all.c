356b5f3adbc4267db80ec8328d316a60a714db4d
SaLaam17 <slaamri@yahoo.fr>
2024-11-21 19:42:24 +0100

    3-print_all.c variadic_functions.h

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -4,12 +4,40 @@
 
 
 /**
- * print_string - 
+ * print_string - function that print a string
+ * @nom_liste: 
 */
 
 void print_string(va_list nom_liste)
 {
-	printf("%s", va_arg(nom_liste, char *);
+	printf("%s", va_arg(nom_liste, char *));
+}
+
+/**
+ * print_int - function that print an int
+ * @nom_liste: 
+*/
+void print_int(va_list nom_liste)
+{
+	printf("%d", va_arg(nom_liste, int));
+}
+
+/**
+ * print_float - function that print a float
+ * @nom_liste: 
+*/
+void print_float(va_list nom_liste)
+{
+	printf("%f", va_arg(nom_liste, float));
+}
+
+/**
+ * print_char - function that print a char
+ * @nom_liste: 
+*/
+void print_char(va_list nom_liste)
+{
+	printf("%s", va_arg(nom_liste, char));
 }
 
  /**
@@ -24,5 +52,5 @@ void print_all(const char * const format, ...)
 	int j =0;
 
 	va_start(nom_liste, format);
-
+}
 
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -4,16 +4,16 @@
 typedef struct name
 {
 	char *letter; 
-	void(*f)void(va_list);
+	void(*f)(va_list);
 } name_name;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void
-void
-void print_float
+void print_char(va_list nom_liste);
+void print_int(va_list nom_liste);
+void print_float(va_list nom_liste);
 void print_string(va_list nom_liste);
 
 #endif
