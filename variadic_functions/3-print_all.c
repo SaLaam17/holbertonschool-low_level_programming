eeb87a3a0aeaefbc7d00a1668412e6897750a083
SaLaam17 <slaamri@yahoo.fr>
2024-11-21 21:55:02 +0100

    add functions

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,55 +2,76 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 
-
 /**
- * print_string - function that print a string
- * @nom_liste: 
+ * print_char - function that print a char
+ * @args: list of arguments
 */
-
-void print_string(va_list nom_liste)
+void print_char(va_list args)
 {
-	printf("%s", va_arg(nom_liste, char *));
+	printf("%c", va_arg(args, char));
 }
 
 /**
  * print_int - function that print an int
- * @nom_liste: 
+ * @args: list of arguments
 */
-void print_int(va_list nom_liste)
+void print_int(va_list args)
 {
-	printf("%d", va_arg(nom_liste, int));
+	printf("%d", va_arg(args, int));
 }
 
 /**
  * print_float - function that print a float
- * @nom_liste: 
+ * @args: list of arguments
 */
-void print_float(va_list nom_liste)
+void print_float(va_list args)
 {
-	printf("%f", va_arg(nom_liste, float));
+	printf("%f", va_arg(args, float));
 }
 
 /**
- * print_char - function that print a char
- * @nom_liste: 
+ * print_string - function that print a string
+ * @args: list of arguments
 */
-void print_char(va_list nom_liste)
+
+void print_string(va_list args)
 {
-	printf("%s", va_arg(nom_liste, char));
+	printf("%s", va_arg(args, char *));
+	
+	if (va_arg(args, char *) == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+
+	printf("%s", va_arg(args, char *));
 }
 
  /**
  * print_all - function that prints anything.
- *
+ * @format: A string of characters representing the argument types.
  */
 
 void print_all(const char * const format, ...)
 {
-	va_list nom_liste;
+	va_list args;
 	int i = 0;
 	int j =0;
+	name_name array []= {
+	{"c", print_char},
+	{"i", print_int},
+	{"f", print_float},
+	{"s", print_string},
+	{NULL, NULL}
+	};
 
-	va_start(nom_liste, format);
-}
+	va_start(args, format);
+
+
+
+
+
+
+	va_end(args);
+
 
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,19 +1,34 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+
+/**
+ * struct name - A new struct type defining a printer.
+ * @letter: 
+ * @f: function pointer to a function that prints
+ * a data type corresponding to letter.
+ */
 
 typedef struct name
 {
 	char *letter; 
-	void(*f)(va_list);
+	void(*f)(va_list args);
 } name_name;
+
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+
+} printer_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void print_char(va_list nom_liste);
-void print_int(va_list nom_liste);
-void print_float(va_list nom_liste);
-void print_string(va_list nom_liste);
+void print_char(va_list args);
+void print_int(va_list args);
+void print_float(va_list args);
+void print_string(va_list args);
 
 #endif
