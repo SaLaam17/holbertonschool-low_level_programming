1f5d56d3559c316f00456ada77ae9226d026d1fb
SaLaam17 <slaamri@yahoo.fr>
2024-11-21 16:05:00 +0000

    creating_print_all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,11 +2,27 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 
+
 /**
+ * print_string - 
+*/
+
+void print_string(va_list nom_liste)
+{
+	printf("%s", va_arg(nom_liste, char *);
+}
+
+ /**
  * print_all - function that prints anything.
  *
  */
 
 void print_all(const char * const format, ...)
 {
+	va_list nom_liste;
+	int i = 0;
+	int j =0;
+
+	va_start(nom_liste, format);
+
 
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,9 +1,19 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+typedef struct name
+{
+	char *letter; 
+	void(*f)void(va_list);
+} name_name;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void
+void
+void print_float
+void print_string(va_list nom_liste);
 
 #endif
