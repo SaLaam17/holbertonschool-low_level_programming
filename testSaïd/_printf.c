8c3198fdcdf5b1cbbf2d14916c4d683b8c2073c7
SaLaam17 <slaamri@yahoo.fr>
2024-11-27 20:16:58 +0100

    _printf.c

--- testSaïd/_printf.c
+++ testSaïd/_printf.c
@@ -11,21 +11,24 @@
 
 int print_char(va_list args)
 {
-	char letter = va_arg(args, int); 
-	_putchar(letter); 
-	return(1); 
+	char letter = va_arg(args, int);
+
+	_putchar(letter);
+	return (1);
 }
 
 /**
  * print_string - function that print a string
  * @args: A va_list pointing to the string to be printed.
+ * Return: the number of characters printed
 */
 
 
-int print_string(va_list args) 
+int print_string(va_list args)
 {
 	int i;
-	char *str = va_arg(args, char *); 
+	char *str = va_arg(args, char *);
+
 	if (str == NULL)
 	{
 		str = "(NULL)";
@@ -36,21 +39,29 @@ int print_string(va_list args)
 		_putchar(str[i]);
 		i++;
 	}
-	return(i);
+	return (i);
 }
+
+/**
+ * print_percent - function that print a percent symbol
+ * @args: A va_list pointing to the string to be printed.
+ * Return: 1
+*/
+
 int print_percent(va_list args)
 {
 	(void)args;
 	_putchar('%');
-	return(1);
+	return (1);
 }
  /**
- * _printf - function that prints ...
- * Description: 
+ * _printf - function that produces output according to a format.
+ * Description: function that produces output according to a format.
  * @format: A string of characters representing the argument types.
+ * Return: The number of characters printed.
  */
 
-int _printf(const char * format, ...)
+int _printf(const char *format, ...)
 {
 	va_list args;
 	int i = 0, j;
@@ -63,30 +74,30 @@ int _printf(const char * format, ...)
 
 	va_start(args, format);
 
-    while (format[i] != '\0') 
+	while (format[i] != '\0')
 	{
-        if (format[i] == '%') 
+		if (format[i] == '%')
 		{
-            i++;
-            j = 0;
-            while (array[j].letter != '\0') 
+			i++;
+			j = 0;
+
+			while (array[j].letter != '\0')
 			{
-                if (format[i] == array[j].letter) 
+				if (format[i] == array[j].letter)
 				{
-                    array[j].function_pointer(args);
-                    break;
-                }
-                j++;
-            }
-        } 
-		else 
+					array[j].function_pointer(args);
+					break;
+				}
+				j++;
+			}
+		}
+		else
 		{
-            _putchar(format[i]);
-        }
-        i++;
-    }
-	
+			_putchar(format[i]);
+		}
+		i++;
+	}
+
 	va_end(args);
 	return (1);
 }
-
