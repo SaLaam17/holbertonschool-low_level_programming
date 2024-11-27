85137f7f98e8f58e9473850974f13b84ef98a41c
SaLaam17 <slaamri@yahoo.fr>
2024-11-27 12:00:17 +0100

    fix gcc error lib unistd

--- _printf/main.h
+++ _printf/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
 
 int _printf(const char *format, ...);
 int _putchar(char c);
@@ -17,4 +20,5 @@ typedef struct type_format {
     char *letter;
     int (*function_pointer)(va_list args);
 }format_t;
+
 #endif
